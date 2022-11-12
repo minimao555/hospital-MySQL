fcf011e8f5e2bc02888e4a06e57a608fb611ac43
Zheng Huang <hz1624917200@outlook.com>
2022-11-12 11:24:26 +0800

    feat: create project, clean deprecated codes

--- Django Todo List/djangoTodolist/tasks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	# root path
-	path('', views.index, name='list'),
-	path('update/<int:update_id>', views.update, name='update'),
-	path('delete/<int:del_id>', views.delete, name='delete')
-]
