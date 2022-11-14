807575b9c596fee717940d2a57a170ccf8638264
AuYang <459461160@qq.com>
2022-11-15 01:41:20 +0800

    开始写前端；配置前端调试页面路由

--- project/hospital/views.py
+++ project/hospital/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return render(request, r'index.html', context={'title': 'Hello Django'})
+
+def ico(request):
+    return HttpResponse(open(r'hospital\templates\ico\logo.png', 'rb').read(), content_type='image/jpg')
+    
